18278470cfb6759a69a22715fb81990bf189fe31
Sanket <sanket.gokule14@gmail.com>
2025-04-28 20:44:34 +0900

    #gitignorecommit

---
+++ app/playground/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def say_hello(request):
+    #return HttpResponse('Hello World')
+    return render(request,'hello.html',{'name': 'Sam'})
+
+# Create your views here.
